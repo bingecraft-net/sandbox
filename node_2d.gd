84d1e26d0c9975d6cc8faa92dad67dfe7fc8b7bb
Tyler <tyler@cruftbusters.com>
2026-06-10 22:06:44 -0700

    better contour tracing and align tile sprites

--- node_2d.gd
+++ node_2d.gd
@@ -5,63 +5,88 @@ var iterations = 0
 var x = 0
 var y = 0
 var iso = 0.
-var direction = Vector2.RIGHT
-
-var left = 0
-var right = 0
-var top = 0
-var bottom = 0
+var direction
 
 @export var noise = FastNoiseLite.new()
 
-@export_tool_button("Regenerate", "Callable") var regenerate_action = regenerate
+@export var step_size = 1_000_000
 
+@export_tool_button("Step", "Callable") var step_action = step
 
-func regenerate():
-	while iterations < 1_000_000:
-		if iterations > 10 and x == 0 and y == 0:
+func step():
+	for k in range(step_size):
+		if iterations > 0 and x == 0 and y == 0:
 			print("done after %d iterations" % iterations)
-			print("bb: %d %d %d %d" % [right, bottom, left, top])
-			break
+			return
+		step_one()
+	print("not done after %d iterations" % iterations)
 
-		var value = noise.get_noise_2d(x, y)
-		var contains = value <= iso
-		var atlas_coords = Vector2i(0, 0) if contains else Vector2i(3, 0)
-		$TileMapLayer.set_cell(Vector2i(x, y), 0, atlas_coords)
-		
-		var angle = PI / 2 if contains else - PI / 2
-		direction = Vector2i(
-			direction.x * cos(angle) - direction.y * sin(angle),
-			direction.x * sin(angle) + direction.y * cos(angle),
-		)
-		var atlas_coords_direction = Vector2i(0, 1)
-		if direction.y > 0.5:
-			atlas_coords_direction = Vector2i(1, 1)
-		elif direction.x < -0.5:
-			atlas_coords_direction = Vector2i(2, 1)
-		elif direction.y < -0.5:
-			atlas_coords_direction = Vector2i(3, 1)
-		$TileMapLayer_Direction.set_cell(Vector2i(x, y), 0, atlas_coords_direction)
-		
-		right = max(right, x)
-		bottom = max(bottom, y)
-		left = min(left, x)
-		top = min(top, y)
-				
-		x = x + direction.x
-		y = y + direction.y
-		iterations += 1
+@export_tool_button("Step One", "Callable") var step_one_action = step_one
+
+var directions = [Vector2i.RIGHT, Vector2i.DOWN, Vector2i.LEFT, Vector2i.UP]
+
+func step_one():
+	var tile_size = $TileMapLayer.tile_set.tile_size
+	var index = 0
+	var values = [
+		noise.get_noise_2d(x * tile_size.x, y * tile_size.y),
+		noise.get_noise_2d((x + 1) * tile_size.x, y * tile_size.y),
+		noise.get_noise_2d((x + 1) * tile_size.x, (y + 1) * tile_size.y),
+		noise.get_noise_2d(x * tile_size.x, (y + 1) * tile_size.y),
+	]
+	if values[0] <= iso:
+		index += 1
+	if values[1] <= iso:
+		index += 2
+	if values[2] <= iso:
+		index += 4
+	if values[3] <= iso:
+		index += 8
+	$TileMapLayer.set_cell(Vector2i(x, y), 0, Vector2i(index % 4, index / 4))
+	
+	if direction == 0:
+		if index & 3 == 1:
+			direction = (4 + direction - 1) % 4
+		elif index & 12 == 4:
+			direction = (direction + 1) % 4
+	elif direction == 1:
+		if index & 6 == 2:
+			direction = (4 + direction - 1) % 4
+		elif index & 9 == 8:
+			direction = (direction + 1) % 4
+	elif direction == 2:
+		if index & 12 == 4:
+			direction = (4 + direction - 1) % 4
+		elif index & 3 == 1:
+			direction = (direction + 1) % 4
+	elif direction == 3:
+		if index & 9 == 8:
+			direction = (4 + direction - 1) % 4
+		elif index & 6 == 2:
+			direction = (direction + 1) % 4
+	
+	#if direction != Vector2i.DOWN and (index & 1 > 0) != (index & 2 > 0):
+	#	direction = Vector2i.UP
+	#elif direction != Vector2i.LEFT and (index & 2 > 0) != (index & 4 > 0):
+	#	direction = Vector2i.RIGHT
+	#elif direction != Vector2i.UP and (index & 4 > 0) != (index & 8 > 0):
+	#	direction = Vector2i.DOWN
+	#elif direction != Vector2i.RIGHT and (index & 8 > 0) != (index & 1 > 0):
+	#	direction = Vector2i.LEFT
+	
+	x = x + directions[direction].x
+	y = y + directions[direction].y
+	iterations += 1
 
 @export_tool_button("Clear", "Callable") var clear_action = clear
 
 func clear():
-	print("clearing")
-
 	iterations = 0
 	x = 0
 	y = 0
-	iso = noise.get_noise_2d(x, y)
-	direction = Vector2.RIGHT
+	iso = noise.get_noise_2d(0, 0)
+	direction = 0
 
 	$TileMapLayer.clear()
-	$TileMapLayer_Direction.clear()
+	
+	print("cleared")
