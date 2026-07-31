2f8f645ac49b9eab83b2e94d72245c5cec5c2cc9
Tyler <tyler@cruftbusters.com>
2026-07-31 15:12:25 -0700

    add superterrain and ore

--- terrain.gd
+++ terrain.gd
@@ -4,7 +4,10 @@ extends Node2D
 var atlas_coords_by_name = {
 	"Empty": Vector2i(0, 0),
 	"Fill": Vector2i(1, 0),
-	"Atmosphere": Vector2i(0, 1),
+	"Atmosphere": Vector2i(2, 0),
+	
+	"Air": Vector2i(0, 1),
+	"Ore": Vector2i(1, 1),
 }
 
 var chunks = {}
@@ -13,12 +16,14 @@ var chunk_size = 16
 var render_size = 5
 
 
+func world_to_chunk_coords(world_coords: Vector2) -> Vector2i:
+	return (world_coords / cell_size / chunk_size - Vector2.ONE / 2).snapped(Vector2.ONE)
+
+
 func load_chunks(world_coords: Vector2) -> void:
-	var top_left_cell_coords = Vector2i((world_coords / cell_size / chunk_size - Vector2.ONE / 2).snapped(Vector2.ONE))
+	var top_left_chunk_coords = world_to_chunk_coords(world_coords) - render_size * Vector2i.ONE / 2
 	for index in range(render_size * render_size):
-		var local_cell_coords = Vector2i(index % render_size, index / render_size)
-		var chunk_coords = top_left_cell_coords + local_cell_coords - render_size * Vector2i.ONE / 2
-		load_chunk(chunk_coords)
+		load_chunk(top_left_chunk_coords + Vector2i(index % render_size, index / render_size))
 
 
 func load_chunk(chunk_coords: Vector2i) -> void:
@@ -26,16 +31,30 @@ func load_chunk(chunk_coords: Vector2i) -> void:
 		for index in range(chunk_size * chunk_size):
 			var local_cell_coords = Vector2i(index % chunk_size, index / chunk_size)
 			var cell_coords = chunk_size * chunk_coords + local_cell_coords
-			var atlas_coords = sample(cell_coords)
+			var atlas_coords = sample_subterrain(cell_coords)
 			$SubTerrain.set_cell(cell_coords, 0, atlas_coords)
+			if atlas_coords == atlas_coords_by_name.get("Fill"):
+				atlas_coords = sample_supterrain(cell_coords)
+				$SupTerrain.set_cell(cell_coords, 0, atlas_coords)
 		chunks[chunk_coords] = 0
 		
 		
-func sample(cell_coords: Vector2) -> Vector2i:
+func sample_subterrain(cell_coords: Vector2) -> Vector2i:
 	var name = "Empty"
-	var magnitude = cell_coords.length() + 16 * sin(8 * cell_coords.angle())
-	if magnitude < 64:
+	var magnitude = cell_coords.length()
+	if magnitude + 16 * sin(8 * cell_coords.angle()) < 128:
 		name = "Fill"
-	elif magnitude < 128:
+	elif magnitude < 128 + 64:
 		name = "Atmosphere"
 	return atlas_coords_by_name.get(name)
+
+var ore_noise = FastNoiseLite.new()
+var ore_iso = 0.2
+
+func sample_supterrain(cell_coords: Vector2) -> Vector2i:
+	var name = "Air"
+	if ore_noise.get_noise_2d(cell_coords.x, cell_coords.y) >= ore_iso:
+		name = "Ore"
+	
+	return atlas_coords_by_name.get(name)
+	
