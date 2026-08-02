d6bdb7094ec8f4d92462b7160c2279b4cd7fd32f
Tyler <tyler@cruftbusters.com>
2026-08-02 14:34:32 -0700

    strip it down to just circular subterrain

--- terrain.gd
+++ terrain.gd
@@ -1,13 +1,8 @@
 extends Node2D
 
-@export
 var atlas_coords_by_name = {
 	"Empty": Vector2i(0, 0),
 	"Fill": Vector2i(1, 0),
-	"Atmosphere": Vector2i(2, 0),
-	
-	"Air": Vector2i(0, 1),
-	"Ore": Vector2i(1, 1),
 }
 
 var chunks = {}
@@ -31,30 +26,15 @@ func load_chunk(chunk_coords: Vector2i) -> void:
 		for index in range(chunk_size * chunk_size):
 			var local_cell_coords = Vector2i(index % chunk_size, index / chunk_size)
 			var cell_coords = chunk_size * chunk_coords + local_cell_coords
-			var atlas_coords = sample_subterrain(cell_coords)
+			var atlas_coords = sample(cell_coords)
 			$SubTerrain.set_cell(cell_coords, 0, atlas_coords)
-			if atlas_coords == atlas_coords_by_name.get("Fill"):
-				atlas_coords = sample_supterrain(cell_coords)
-				$SupTerrain.set_cell(cell_coords, 0, atlas_coords)
 		chunks[chunk_coords] = 0
 		
 		
-func sample_subterrain(cell_coords: Vector2) -> Vector2i:
+func sample(cell_coords: Vector2) -> Vector2i:
 	var name = "Empty"
 	var magnitude = cell_coords.length()
-	if magnitude + 16 * sin(8 * cell_coords.angle()) < 128:
+	if magnitude < 128:
 		name = "Fill"
-	elif magnitude < 128 + 64:
-		name = "Atmosphere"
-	return atlas_coords_by_name.get(name)
-
-var ore_noise = FastNoiseLite.new()
-var ore_iso = 0.2
-
-func sample_supterrain(cell_coords: Vector2) -> Vector2i:
-	var name = "Air"
-	if ore_noise.get_noise_2d(cell_coords.x, cell_coords.y) >= ore_iso:
-		name = "Ore"
-	
 	return atlas_coords_by_name.get(name)
 	
