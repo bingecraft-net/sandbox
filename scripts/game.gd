ede3c68d82815852e509b3c5a1d3fda10883587a
Tyler <tyler@cruftbusters.com>
2026-09-12 23:05:43 -0700

    add energy diffuse and mass condensation

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-@export var gravity_constant: float = 0.01
 @export var grid_size: int = 64
 @export var noise: FastNoiseLite = FastNoiseLite.new()
+@export var energy_density_average = 0.75
+@export var energy_density_deviation = 0.5
 
 @onready var tile_map_layer: TileMapLayer = $TileMapLayer
 
@@ -15,7 +16,7 @@ func _ready() -> void:
 	for x in range(grid_size):
 		grid.append([])
 		for y in range(grid_size):
-			var energy_density = noise.get_noise_2d(x, y) + 1
+			var energy_density = energy_density_average + noise.get_noise_2d(x, y) * energy_density_deviation
 			var value = Cell.new(energy_density, 0.)
 			grid[x].append(value)
 
@@ -25,11 +26,16 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
+	var e_total = 0
 	var next_grid: Array[Array] = []
 	for x in range(grid_size):
 		next_grid.append([])
 		for y in range(grid_size):
 			var current_value: Cell = grid[x][y]
+			var e = current_value.energy_density
+			var m = current_value.mass_density
+			
+			var laplacian = 0.
 			for dx in range(-1, 2):
 				for dy in range(-1, 2):
 					if dx == 0 and dy == 0:
@@ -37,16 +43,21 @@ func _process(delta: float) -> void:
 					var neighbor_x = (x + dx + grid_size) % grid_size
 					var neighbor_y = (y + dy + grid_size) % grid_size
 					var neighbor_value = grid[neighbor_x][neighbor_y]
+					laplacian += (neighbor_value.energy_density - current_value.energy_density) / 8.
+			
+			e += laplacian / 8.
+			
+			if e > 1. and e < 1.001:
+				e -= 1.
+				m += 1.
 
-			if current_value.energy_density > 1. and current_value.energy_density < 1.01:
-				next_grid[x].append(Cell.new(current_value.energy_density - 1, current_value.mass_density + 1))
-			else:
-				next_grid[x].append(current_value)
+			next_grid[x].append(Cell.new(e, m))
+			e_total += e
 
 			var coords = crs.forward(x, y)
 			var atlas_coords = classify(current_value)
 			tile_map_layer.set_cell(coords, 0, atlas_coords)
-	
+
 	grid = next_grid
 
 
