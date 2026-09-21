5755f1f9238a2917123cfed0b1f52def0e8aa2da
arctair <arctair>
2026-09-20 17:20:09 -0700

    add random mass to cells

--- scripts/cell.gd
+++ scripts/cell.gd
@@ -6,22 +6,27 @@ extends Node2D
 
 @onready var map: TileMapLayer = $TileMapLayer
 
+var elapsed = 0
+var mass = Array()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-var elapsed = 0
+	for x in range(grid_size):
+		mass.push_back([])
+		for y in range(grid_size):
+			mass[x].push_back(randf())
 
 func _process(delta: float) -> void:
 	elapsed += delta
 	for x in range(grid_size):
 		for y in range(grid_size):
-			var value = noise.get_noise_3d(x + offset.x, y + offset.y, elapsed)
-			var atlas_coords = classify(value)
+			var a = noise.get_noise_3d(x + offset.x, y + offset.y, elapsed)
+			var b = mass[x][y]
+			var atlas_coords = classify(a, b)
 			map.set_cell(Vector2i(x, y), 0, atlas_coords)
 
-func classify(value: float) -> Vector2i:
-	var fx = atan(value * 24)
+func classify(a: float, b: float) -> Vector2i:
+	var fx = atan(a * 24)
 	var gx = cos(fx)
 	var magnitude = 16 * gx
-	return floor(magnitude) * Vector2i.RIGHT
+	return floor(magnitude) * Vector2i.RIGHT + floor(9 * b) * Vector2i.DOWN
