b4ae1d158c03a84d69dda719d2746a2d6f79f42e
Tyler <tyler@cruftbusters.com>
2026-09-23 19:10:24 -0700

    smooth transfer with MASS_GAIN=0.35

--- scenes/cell.gd
+++ scenes/cell.gd
@@ -9,6 +9,7 @@ extends Node2D
 var elapsed = 0
 var mass = Array()
 var mode = true
+const MASS_GAIN := 0.35
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,14 +25,14 @@ func _process(delta: float) -> void:
 		for y in range(grid_size):
 			var magnitude = 0.
 			if mode:
-				magnitude = 15 * mass[x][y]
+				magnitude = 15.0 * mass[x][y]
 			else:
-				magnitude = 16 * sample(x, y, elapsed)
+				magnitude = 16.0 * sample(x, y, elapsed)
 			map.set_cell(Vector2i(x, y), 0, floor(magnitude) * Vector2i.RIGHT)
 
 
-func sample(x: float, y: float, elapsed: float) -> float:
-	var a = noise.get_noise_3d(x + offset.x, y + offset.y, elapsed)
+func sample(x: float, y: float, time: float) -> float:
+	var a = noise.get_noise_3d(x + offset.x, y + offset.y, time)
 	var fx = atan(a * 24)
 	var gx = 1 - cos(fx)
 	return gx
@@ -61,7 +62,13 @@ func _on_timer_timeout() -> void:
 				
 				var mean = (m00 + m10 + m11 + m01 + a00 + a10 + a11 + a01) / 4.
 				
-				mass[x0][y0] += (mean - m00 - a00)
-				mass[x1][y0] += (mean - m10 - a10)
-				mass[x1][y1] += (mean - m11 - a11)
-				mass[x0][y1] += (mean - m01 - a01)
+				m00 += (mean - m00 - a00) * MASS_GAIN
+				m10 += (mean - m10 - a10) * MASS_GAIN
+				m11 += (mean - m11 - a11) * MASS_GAIN
+				m01 += (mean - m01 - a01) * MASS_GAIN
+				
+				mass[x0][y0] = m00
+				mass[x1][y0] = m10
+				mass[x1][y1] = m11
+				mass[x0][y1] = m01
+				
