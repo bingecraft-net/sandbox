edc50d199beaf887efff4c19a0083d8637643e73
Tyler <tyler@cruftbusters.com>
2026-09-23 18:53:43 -0700

    step both frames every tick

--- scenes/cell.gd
+++ scenes/cell.gd
@@ -40,29 +40,28 @@ func _on_node_2d_character_on_mode_change() -> void:
 	mode = not mode
 
 
-var tick = 0
 func _on_timer_timeout() -> void:
-	tick += 1
-	for x in range(tick % 2, grid_size, 2):
-		for y in range(tick % 2, grid_size, 2):
-			var x0 = x
-			var x1 = (x + 1) % grid_size
-			var y0 = y
-			var y1 = (y + 1) % grid_size
-			
-			var m00 = mass[x0][y0]
-			var m10 = mass[x1][y0]
-			var m11 = mass[x1][y1]
-			var m01 = mass[x0][y1]
-			
-			var a00 = sample(x0, y0, elapsed)
-			var a10 = sample(x1, y0, elapsed)
-			var a11 = sample(x1, y1, elapsed)
-			var a01 = sample(x0, y1, elapsed)
-			
-			var mean = (m00 + m10 + m11 + m01 + a00 + a10 + a11 + a01) / 4.
-			
-			mass[x0][y0] += (mean - m00 - a00)
-			mass[x1][y0] += (mean - m10 - a10)
-			mass[x1][y1] += (mean - m11 - a11)
-			mass[x0][y1] += (mean - m01 - a01)
+	for frame in range(2):
+		for x in range(frame, grid_size, 2):
+			for y in range(frame, grid_size, 2):
+				var x0 = x
+				var x1 = (x + 1) % grid_size
+				var y0 = y
+				var y1 = (y + 1) % grid_size
+				
+				var m00 = mass[x0][y0]
+				var m10 = mass[x1][y0]
+				var m11 = mass[x1][y1]
+				var m01 = mass[x0][y1]
+				
+				var a00 = sample(x0, y0, elapsed)
+				var a10 = sample(x1, y0, elapsed)
+				var a11 = sample(x1, y1, elapsed)
+				var a01 = sample(x0, y1, elapsed)
+				
+				var mean = (m00 + m10 + m11 + m01 + a00 + a10 + a11 + a01) / 4.
+				
+				mass[x0][y0] += (mean - m00 - a00)
+				mass[x1][y0] += (mean - m10 - a10)
+				mass[x1][y1] += (mean - m11 - a11)
+				mass[x0][y1] += (mean - m01 - a01)
