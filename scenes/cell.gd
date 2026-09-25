d56a61c0b78c96642b9438ce929cbabdb74ae306
Tyler <tyler@cruftbusters.com>
2026-09-25 16:41:02 -0700

    add sliders

--- scenes/cell.gd
+++ scenes/cell.gd
@@ -2,18 +2,30 @@ extends Node2D
 
 @export var noise: FastNoiseLite = FastNoiseLite.new()
 @export var grid_size: int = 128
+@export var valleyness: float = 0.5
 @export var offset: Vector2i = Vector2i.ZERO
 
 @onready var map: TileMapLayer = $TileMapLayer
-@onready var label: RichTextLabel = $CanvasLayer/RichTextLabel
+@onready var label: Label = $CanvasLayer/LayerLabel
+@onready var valleyness_slider: HSlider = $CanvasLayer/ValleynessSlider
+@onready var valleyness_label: Label = $CanvasLayer/ValleynessLabel
+@onready var frequency_slider: HSlider = $CanvasLayer/FrequencySlider
+@onready var frequency_label: Label = $CanvasLayer/FrequencyLabel
+@onready var lacunarity_slider: HSlider = $CanvasLayer/LacunaritySlider
+@onready var lacunarity_label: Label = $CanvasLayer/LacunarityLabel
+@onready var mass_gain_slider: HSlider = $CanvasLayer/MassGainSlider
+@onready var mass_gain_label: Label = $CanvasLayer/MassGainLabel
 
 var elapsed = 0
 var mass = Array()
 var mode = true
-const MASS_GAIN := 0.2
+var MASS_GAIN := 0.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	frequency_slider.value_changed.connect(_on_frequency_slider_value_changed)
+	valleyness_slider.value_changed.connect(_on_valleyness_slider_value_changed)
+	lacunarity_slider.value_changed.connect(_on_lacunarity_slider_value_changed)
 	for x in range(grid_size):
 		mass.push_back([])
 		for y in range(grid_size):
@@ -21,7 +33,12 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	elapsed += delta
-	label.text = "mode: %s" % ("mass" if mode else "base")
+	label.text = "Layer: %s" % ("mass (1)" if mode else "base (0)")
+	valleyness_label.text = "Valleyness (%.2f)" % valleyness
+	frequency_label.text = "Frequency (%.4f)" % noise.frequency
+	lacunarity_label.text = "Lacunarity (%.2f)" % noise.fractal_lacunarity
+	mass_gain_label.text = "Mass gain (%.2f)" % MASS_GAIN
+
 
 	for x in range(grid_size):
 		for y in range(grid_size):
@@ -34,12 +51,27 @@ func _process(delta: float) -> void:
 
 
 func sample(x: float, y: float, time: float) -> float:
-	var a = noise.get_noise_3d(x + offset.x, y + offset.y, time)
-	return pow(abs(sin(PI * a)), 0.5)
+	var a = noise.get_noise_3d((x + offset.x), (y + offset.y), time)
+	return pow(abs(sin(PI * a)), valleyness)
 
 
 func _on_node_2d_character_on_mode_change() -> void:
 	mode = not mode
+
+
+func _on_valleyness_slider_value_changed(value: float) -> void:
+	valleyness = pow(2, value - 6)
+
+
+func _on_frequency_slider_value_changed(value: float) -> void:
+	noise.frequency = pow(1.002, value) - 1
+
+
+func _on_lacunarity_slider_value_changed(value: float) -> void:
+	noise.fractal_lacunarity = value / 2.5
+	
+func _on_mass_gain_slider_value_changed(value: float) -> void:
+	MASS_GAIN = value / 10.
 
 
 func _on_timer_timeout() -> void:
